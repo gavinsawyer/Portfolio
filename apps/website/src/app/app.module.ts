b6407d5322dbb06c08fa6e55782d5f793322d530
gavinsawyer <gavin@sawyerfamily.org>
2022-11-19 09:31:22 -0500

    Initial commit

---
+++ apps/website/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule }      from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule }  from "@angular/router";
+import { AppComponent }  from "./app.component";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], {
+      initialNavigation: "enabledBlocking"
+    }),
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent,
+  ],
+})
+export class AppModule {}
