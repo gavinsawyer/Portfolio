ee62904e13b7209aa67a02b5c7d2d51ae8fd61ca
gavinsawyer <gavin@sawyerfamily.org>
2023-01-12 01:46:46 -0500

    Style changes, added all Focus icons, added comments in Cloud Functions

--- functions/focus/reset.js
+++ functions/focus/reset.js
@@ -6,11 +6,11 @@ exports
   .default = functions
   .https
   .onRequest((request, response) => request.body["ShortcutsAPIKey"] === process.env["ShortcutsAPIKey"] ? ((firestore) => ((collectionReference) => ((documentReference) => documentReference.get().then((documentSnapshot) => ((updateData) => documentReference.update(updateData).then(() => ((_response) => ((_promise) => {})(collectionReference.doc(process.env["ShortcutsAPIPublicDocumentID"]).update({
-    "focus": documentSnapshot.data()["focusPrior"],
+    "focus": documentSnapshot.data()["focusPrior"], // Public document new data
   })))(response.json({
     ...documentSnapshot.data(),
     ...updateData,
   }).end())))({
-    "focus": documentSnapshot.data()["focusPrior"],
-    "focusPrior": documentSnapshot.data()["focus"],
+    "focus": documentSnapshot.data()["focusPrior"], // Private document new data
+    "focusPrior": documentSnapshot.data()["focus"], // Private document new data
   })))(collectionReference.doc(process.env["ShortcutsAPIPrivateDocumentID"])))(firestore.collection("_")))(firestore.getFirestore()) : ((_response) => {})(response.status(403).end()));
--- functions/focus/set.js
+++ functions/focus/set.js
@@ -6,11 +6,11 @@ exports
   .default = functions
   .https
   .onRequest((request, response) => request.body["ShortcutsAPIKey"] === process.env["ShortcutsAPIKey"] && typeof request.body["focus"] === "string" ? ((firestore) => ((collectionReference) => ((documentReference) => documentReference.get().then((documentSnapshot) => documentSnapshot.data()["focus"] === request.body["focus"] ? ((_response) => {})(response.json(documentSnapshot.data()).end()) : ((updateData) => documentReference.update(updateData).then(() => ((_response) => ((_promise) => {})(collectionReference.doc(process.env["ShortcutsAPIPublicDocumentID"]).update({
-    "focus": request.body["focus"],
+    "focus": request.body["focus"], // Public document new data
   })))(response.json({
     ...documentSnapshot.data(),
     ...updateData,
   }).end())))({
-    "focus": request.body["focus"],
-    "focusPrior": documentSnapshot.data()["focus"],
+    "focus": request.body["focus"],                 // Private document new data
+    "focusPrior": documentSnapshot.data()["focus"], // Private document new data
   })))(collectionReference.doc(process.env["ShortcutsAPIPrivateDocumentID"])))(firestore.collection("_")))(firestore.getFirestore()) : ((_response) => {})(response.status(403).end()));
