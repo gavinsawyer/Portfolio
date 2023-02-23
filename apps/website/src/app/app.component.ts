71e4a84b64760707fbfdb61f5b0a84b4046f5c13
gavinsawyer <gavin@sawyerfamily.org>
2023-02-23 16:45:10 -0500

    Aside component is hidden except on homepage or when space is available.

--- apps/website/src/app/app.component.ts
+++ apps/website/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component }                  from "@angular/core";
-import { ResponsivityService }        from "@portfolio/services";
+import { Component }                                                                                                                                                                                       from "@angular/core";
+import { ActivationEnd, ActivationStart, ChildActivationEnd, ChildActivationStart, GuardsCheckEnd, GuardsCheckStart, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, ResolveEnd, ResolveStart, RouteConfigLoadEnd, RouteConfigLoadStart, Router, RouterEvent, RoutesRecognized, Scroll } from "@angular/router";
+import { ResponsivityService }                 from "@portfolio/services";
+import { BehaviorSubject, filter, Observable } from "rxjs";
 
 
 @Component({
@@ -10,12 +12,31 @@ import { ResponsivityService }        from "@portfolio/services";
 export class AppComponent {
 
   constructor(
+    Router: Router,
     ResponsivityService: ResponsivityService,
   ) {
     this
       .responsivityService = ResponsivityService;
+
+    this
+      .urlSubject = new BehaviorSubject<string>(Router.url);
+    this
+      .urlObservable = this
+      .urlSubject;
+    this
+      .unsubscribeRouterEvents = Router
+      .events
+      .pipe(
+        filter<RouterEvent | NavigationStart | NavigationEnd | NavigationCancel | NavigationError | RoutesRecognized | GuardsCheckStart | GuardsCheckEnd | RouteConfigLoadStart | RouteConfigLoadEnd | ChildActivationStart | ChildActivationEnd | ActivationStart | ActivationEnd | Scroll | ResolveStart | ResolveEnd, NavigationEnd>((routerEvent: RouterEvent | NavigationStart | NavigationEnd | NavigationCancel | NavigationError | RoutesRecognized | GuardsCheckStart | GuardsCheckEnd | RouteConfigLoadStart | RouteConfigLoadEnd | ChildActivationStart | ChildActivationEnd | ActivationStart | ActivationEnd | Scroll | ResolveStart | ResolveEnd): routerEvent is NavigationEnd => routerEvent instanceof NavigationEnd)
+      )
+      .subscribe((navigationEnd) => this.urlSubject.next(navigationEnd.url))
+      .unsubscribe;
   }
 
+  private readonly urlSubject: BehaviorSubject<string>;
+  private readonly unsubscribeRouterEvents: () => void;
+
   public readonly responsivityService: ResponsivityService;
+  public readonly urlObservable: Observable<string>;
 
 }
