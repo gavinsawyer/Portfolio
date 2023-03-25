d470896108dc5619924254144fadebe4bfe7d45f
gavinsawyer <gavin@sawyerfamily.org>
2023-03-24 20:59:39 -0400

    Moved to 15.11.2
    - Added "Confirm with passkey" button to Console app to test new ngxFirebaseWebAuthn functionality.

--- libs/components/src/lib/aside-console/aside-console.component.ts
+++ libs/components/src/lib/aside-console/aside-console.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule }               from "@angular/common";
-import { Component }                  from "@angular/core";
-import { Auth, UserCredential }       from "@angular/fire/auth";
-import { Functions }                  from "@angular/fire/functions";
-import { signInWithPasskey }          from "@ngx-firebase-web-authn/browser";
-import { ButtonComponent }            from "../button/button.component";
-import { CreateAccountFormComponent } from "../create-account-form/create-account-form.component";
+import { CommonModule }                              from "@angular/common";
+import { Component }                                 from "@angular/core";
+import { Auth, UserCredential }                      from "@angular/fire/auth";
+import { Functions }                                 from "@angular/fire/functions";
+import { signInWithPasskey, confirmUserWithPasskey } from "@ngx-firebase-web-authn/browser";
+import { ButtonComponent }                           from "../button/button.component";
+import { CreateAccountFormComponent }                from "../create-account-form/create-account-form.component";
 
 
 @Component({
@@ -27,10 +27,14 @@ export class AsideConsoleComponent {
     private readonly functions: Functions,
   ) {
     this
+      .confirmUserWithPasskey = (): Promise<void> => confirmUserWithPasskey(auth, functions)
+      .then<void>((): void => void(0));
+    this
       .signInWithPasskey = (): Promise<void> => signInWithPasskey(auth, functions)
       .then<void>((_userCredential: UserCredential): void => void(0));
   }
 
+  public readonly confirmUserWithPasskey: () => Promise<void>;
   public readonly signInWithPasskey: () => Promise<void>;
 
 }
