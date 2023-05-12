01484630cd08eda29099acd497c8a34d1983b4c1
gavinsawyer <g@vinsawyer.dev>
2023-05-12 17:21:42 -0400

    v16.1.4
    - Now using FirebaseWebAuthn from Extensions Hub source
    - Updated dependencies

--- libs/functions/src/lib/shortcuts-api.ts
+++ libs/functions/src/lib/shortcuts-api.ts
@@ -2,6 +2,7 @@ import { getShortcutsApi } from "@gavinsawyer/shortcuts-api";
 import { HttpsFunction }   from "firebase-functions";
 
 
+// noinspection JSUnusedGlobalSymbols
 export const shortcutsAPI: HttpsFunction = getShortcutsApi({
   environmentCollectionPath: "shortcutsEnvironment",
 });
